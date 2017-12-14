f60a9b295f55d1b3b71ca7038212f600aa52478a
Mohammad Mohsen Ghannam <mohammad.mohsen@hotmail.com>
2017-12-14 17:33:30 +0200

    vectorize tweets function

--- preprocessor/data_reader.py
+++ preprocessor/data_reader.py
@@ -1,5 +1,4 @@
 from glob import glob
-from pprint import pprint
 
 
 def get_path(emotion, data_pool='train'):
@@ -16,13 +15,17 @@ def get_path(emotion, data_pool='train'):
 
 
 def read_tweets(path):
+    """
+    generates tweets from path 
+    :param path: string 
+    :return: generator object to be used to iterate over data=[(tweet,level)]
+    """
     with open(path, 'rb') as f:
         # skip first line
         f.readline()
         while f:
             line = str(f.readline())
             splitted = line.split('\\t')
-            print(splitted)
 
             # check for end of input
             if len(splitted) == 4:
@@ -34,6 +37,44 @@ def read_tweets(path):
             yield (tweet, level)
 
 
+def vectorize(tweets, method='count', stemming=True, no_stopwords=True, no_mentions=True):
+    """
+    vectorizes a list tweets 
+    :param tweets: a list of tweets 
+    :param method: vectorizing type
+    :param stemming: use stem of the words in a tweet
+    :param no_stopwords: removes low information words if True
+    :param no_mentions: removes mentions if true
+    :return: returns a sparse matrix of vectorized tweets
+    """
+
+    def __count_vectorize(tweets):
+        from sklearn.feature_extraction.text import CountVectorizer
+        return CountVectorizer().fit_transform(tweets)
+
+    if stemming or no_mentions or no_mentions:
+        from nltk.stem import SnowballStemmer
+        from nltk.corpus import stopwords
+
+        stemmer = SnowballStemmer('english')
+        sw = stopwords.words('english')
+        for i, tweet in enumerate(tweets):
+            tweet_words = tweet.split(' ')
+            if stemming:
+                tweet_words = [stemmer.stem(word) for word in tweet_words]
+            if no_mentions:
+                tweet_words = [word for word in tweet_words if word and word[0] != '@']
+            if no_stopwords:
+                tweet_words = [word for word in tweet_words if word not in sw]
+            tweets[i] = ' '.join(tweet_words)
+
+    if method == 'count':
+        return __count_vectorize(tweets)
+
+        ## TODO(1): add other vectorizers
+
+
 if __name__ == '__main__':
     path = get_path('joy')
-    pprint([x for x in (read_tweets(path))])
+    tweets = [x[0] for x in (read_tweets(path))]
+    print(vectorize(tweets).shape)
