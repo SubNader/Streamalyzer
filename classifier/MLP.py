fa30da20eeefb204107b0a809d473fce6f33263c
Mohamed Gamal <jimadness12@gmail.com>
2017-12-17 18:00:18 +0200

    Fixed classifier to train on training set and test on dev set

--- classifier/MLP.py
+++ classifier/MLP.py
@@ -3,7 +3,7 @@ from sklearn.neural_network import MLPClassifier
 
 
 def run(emotion):
-    training_vectors, test_vectors, training_labels, test_labels = fetch_data(emotion, 'train')
+    training_vectors, test_vectors, training_labels, test_labels = fetch_data(emotion)
     classifier = MLPClassifier()
     classifier.fit(training_vectors, training_labels)
     print('MLP Accuracy:', classifier.score(test_vectors, test_labels))
--- classifier/adaboost.py
+++ classifier/adaboost.py
@@ -3,7 +3,7 @@ from sklearn.ensemble import AdaBoostClassifier
 
 
 def run(emotion):
-    training_vectors, test_vectors, training_labels, test_labels = fetch_data(emotion, 'train')
+    training_vectors, test_vectors, training_labels, test_labels = fetch_data(emotion)
     classifier = AdaBoostClassifier()
     classifier.fit(training_vectors, training_labels)
     print('AdaBoost Accuracy:', classifier.score(test_vectors, test_labels))
