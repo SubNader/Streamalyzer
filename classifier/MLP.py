1cd8ca1e703ce8b881000040d952cba05f5d28a0
Mohamed Gamal <jimadness12@gmail.com>
2017-12-19 21:38:34 +0200

    Added some parameters for adaboost, KNN, and MLP classifiers.

--- classifier/MLP.py
+++ classifier/MLP.py
@@ -4,6 +4,8 @@ from sklearn.neural_network import MLPClassifier
 
 def run(emotion):
     training_vectors, test_vectors, training_labels, test_labels = fetch_data(emotion)
-    classifier = MLPClassifier()
-    classifier.fit(training_vectors, training_labels)
-    print('MLP Accuracy:', classifier.score(test_vectors, test_labels))
+    alphas = [0.0001, 0.001, 0.01, 0.1]
+    for a in alphas:
+        classifier = MLPClassifier(alpha=a)
+        classifier.fit(training_vectors, training_labels)
+        print('MLP Accuracy (alpha ', a, '):', classifier.score(test_vectors, test_labels))
