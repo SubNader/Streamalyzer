db921936c4679b5058fafc06cb084025d5b47a1f
Mohamed Gamal <jimadness12@gmail.com>
2017-12-22 17:57:08 +0200

    Added plotting benchmarks.

--- classifier/benchmark.py
+++ classifier/benchmark.py
@@ -1,12 +1,26 @@
 from classifier import adaboost, KNN, MLP, random_forest, naive_bayes
+import matplotlib.pyplot as plt
 
 if __name__ == '__main__':
     emotions = ['joy', 'fear', 'anger', 'sadness']
+    plt.title('AdaBoost learning rates vs Accuracies')
     for e in emotions:
-        print('Printing accuracies for:', e)
         adaboost.run(e)
+    plt.legend()
+    plt.show()
+    plt.title('KNN n_neighbors vs Accuracies')
+    for e in emotions:
         KNN.run(e)
+    plt.legend()
+    plt.show()
+    plt.title('MLP alphas vs Accuracies')
+    for e in emotions:
         MLP.run(e)
+    plt.legend()
+    plt.show()
+    for e in emotions:
         random_forest.run(e)
+    # plt.show()
+    for e in emotions:
         naive_bayes.run(e)
-        print('-----------------------------\n')
+        # plt.show()
